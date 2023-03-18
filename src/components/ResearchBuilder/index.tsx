ad4dd3cf28e6b182e1fc99465a5649f88ad1eb09
Oguz Ongel <ongelo@ongelo.local>
2023-03-18 12:44:25 -0500

    Redesign of the research home.

--- src/components/ResearchBuilder/index.tsx
+++ src/components/ResearchBuilder/index.tsx
@@ -1,5 +1,5 @@
 import { GeneratedFormField } from "@/backend/generateResearch";
-import { Button, Group } from "@mantine/core";
+import { Button, Group, Stack, Text, Title } from "@mantine/core";
 import { useLocalStorage } from "@mantine/hooks";
 import { useState } from "react";
 import { BlockType } from "../enums";
@@ -58,25 +58,36 @@ const ResearchBuilder = () => {
 
   if (buildOption === null) {
     return (
-      <Group spacing="lg" position="center" pt="lg">
-        <Button
-          color="violet"
-          size="lg"
-          onClick={() => setBuildOption(BuildOption.Ai)}
-        >
-          Generate with AI
-        </Button>
-        <Button size="lg" onClick={() => setBuildOption(BuildOption.New)}>
-          Create manually
-        </Button>
-        <Button
-          variant="outline"
-          size="lg"
-          onClick={() => setBuildOption(BuildOption.Load)}
-        >
-          Load previous
-        </Button>
-      </Group>
+      <Stack>
+        <Stack align="center">
+          <Title>Create a research survey</Title>
+          <Text>
+            Create a research survey that you can share out to gather data from
+            people.
+          </Text>
+        </Stack>
+        <Group spacing="lg" position="center" pt="lg">
+          <Button
+            color="violet"
+            size="lg"
+            onClick={() => setBuildOption(BuildOption.Ai)}
+          >
+            Generate with AI
+          </Button>
+          <Button size="lg" onClick={() => setBuildOption(BuildOption.New)}>
+            Create manually
+          </Button>
+        </Group>
+        <Group spacing="lg" position="center">
+          <Button
+            variant="subtle"
+            size="sm"
+            onClick={() => setBuildOption(BuildOption.Load)}
+          >
+            Load previous
+          </Button>
+        </Group>
+      </Stack>
     );
   }
 
